773d601b378730ca337f3234458463bfbaf7e6ad
LuisVillaC <luiscv.ingeniero@gmail.com>
2023-06-25 15:56:29 -0500

    feat: added endpoint to list visits by params

--- backend/apps/visits/views.py
+++ backend/apps/visits/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+import datetime
+
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
+
+from .models import Visit
+from .serializers import VisitSerializer
+from rest_framework.decorators import action
+
 
 # Create your views here.
+class VisitViewSet(mixins.DestroyModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    """
+    Visit endpoints
+    """
+    queryset = Visit.objects.all()
+    serializer_class = VisitSerializer
+
+    def list(self, request):
+        start_date = request.GET.get('start_date')
+        end_date = request.GET.get('end_date')
+
+        if not request.GET.get('start_date') or not request.GET.get('end_date'):
+            return Response({
+                'error': 'start_date and end_date are required'
+            }, 400)
+
+        try:
+            datetime.date.fromisoformat(start_date)
+            datetime.date.fromisoformat(end_date)
+        except ValueError:
+            return Response({
+                'error': 'start_date and end_date are wrongly formatted'
+            }, 400)
+
+        visits = Visit.objects.filter(created_at__range=[start_date, end_date])
+
+        serializer = VisitSerializer(visits, many=True)
+        return Response(serializer.data)
