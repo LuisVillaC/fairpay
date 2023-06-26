6d12ba4acf51efe2453742e89d13df311fb44b92
LuisVillaC <luiscv.ingeniero@gmail.com>
2023-06-26 13:19:38 -0500

    feat: added waiter context

---
+++ frontend/src/hooks/useLocalStorage.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export const useLocalStorage = (
+  key: string,
+  initialValues: unknown
+): [unknown, React.Dispatch<React.SetStateAction<unknown>>] => {
+  const [state, setState] = useState<unknown>(initialValues);
+  useEffect(() => {
+    const item: string | null = localStorage.getItem(key);
+    if (item) {
+      const parsedItem = JSON.parse(item) || {};
+      if (Object.keys(parsedItem).length > 0) setState(parsedItem);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+};
--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,16 +1,18 @@
 import React from "react";
+
 const DashboardLayout: React.FC<React.PropsWithChildren> = (props) => {
   const { children } = props;
-
   return (
     <>
       <header className="flex items-center justify-between bg-gray-900 px-0 pl-4">
         <div className="flex items-center">
-          <img src="logo_fairpay.png" alt="Logo" className="h-1/4 w-2/6" />
+          <a href="/" className="contents">
+            <img src="/logo_fairpay.png" alt="Logo" className="h-1/4 w-2/6" />
+          </a>
         </div>
         <div className="hidden items-center justify-end md:flex ">
           <img
-            src="fair_pay_header.jpg"
+            src="/fair_pay_header.jpg"
             className="h-[11rem] w-[26rem] rounded-tr-3xl"
             alt="right-header"
           />
