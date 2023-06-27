124429170f8692ceb3ca6438c3fd17926f480bc7
LuisVillaC <luiscv.ingeniero@gmail.com>
2023-06-27 17:54:01 -0500

    feat: added currency format util

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -5,3 +5,11 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
+
+export const currencyFormatter = (value: number) => {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(value);
+};
